5d19855cdc3ac34ee3f61071d288a0ae7acc06da
tomimandalaputra <tommymandalaputra@gmail.com>
2025-06-07 09:27:21 +0700

    Initial Commit

---
+++ Stridez/StridezApp.swift
@@ -0,0 +1,17 @@
+//
+//  StridezApp.swift
+//  Stridez
+//
+//  Created by tukucode on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct StridezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
