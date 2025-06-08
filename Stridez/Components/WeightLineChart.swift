173cbac7817d1c59116496d9a6622a060100b810
tomimandalaputra <tommymandalaputra@gmail.com>
2025-06-08 10:16:28 +0700

    chore: weight line chart styling

--- Stridez/Components/WeightLineChart.swift
+++ Stridez/Components/WeightLineChart.swift
@@ -12,6 +12,10 @@ struct WeightLineChart: View {
 	var selectedTab: HealthMetricContext
 	var chartData: [HealthMetric]
 
+	var minValue: Double {
+		chartData.map { $0.value }.min() ?? 0
+	}
+
 	var body: some View {
 		VStack {
 			NavigationLink(value: selectedTab) {
@@ -34,20 +38,46 @@ struct WeightLineChart: View {
 			.padding(.bottom, 12)
 
 			Chart {
+				RuleMark(y: .value("Goal", 155))
+					.foregroundStyle(Color.mint)
+					.lineStyle(.init(lineWidth: 1, dash: [5]))
+					.annotation(alignment: .leading) {
+						Text("Goal")
+							.font(.caption)
+							.foregroundStyle(Color.secondary)
+					}
+
 				ForEach(chartData) { weights in
 					AreaMark(
 						x: .value("Day", weights.date, unit: .day),
-						y: .value("Value", weights.value)
+						yStart: .value("Value", weights.value),
+						yEnd: .value("Min Value", minValue)
 					)
-					.foregroundStyle(Gradient(colors: [.blue.opacity(0.5), .clear]))
+					.foregroundStyle(Gradient(colors: [.indigo.opacity(0.5), .clear]))
+					.interpolationMethod(.catmullRom)
 
 					LineMark(
 						x: .value("Day", weights.date, unit: .day),
 						y: .value("Value", weights.value)
 					)
+					.foregroundStyle(Color.indigo)
+					.interpolationMethod(.catmullRom)
+					.symbol(.circle)
 				}
 			}
 			.frame(height: 150)
+			.chartYScale(domain: .automatic(includesZero: false))
+			.chartXAxis {
+				AxisMarks {
+					AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
+				}
+			}
+			.chartYAxis {
+				AxisMarks { _ in
+					AxisGridLine().foregroundStyle(Color.secondary.opacity(0.3))
+					AxisValueLabel()
+				}
+			}
 		}
 		.padding()
 		.background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemFill)))
